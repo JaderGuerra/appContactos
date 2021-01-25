b86a698bd3b917d4418129fae410372f26e6ce64
Jader <swilor29@gmail.com>
2021-01-25 17:24:53 -0500

    alerta implementada, rama lista para hacer merge a master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,38 +2,48 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
-  
-  { 
-    path: 'home', 
-    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) 
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
   },
-  { 
-    path: 'contactList', 
-    loadChildren: () => import('./pages/contact-list/contact-list.module').then(m => m.ContactListModule) 
-  },  
+  {
+    path: 'contactList',
+    loadChildren: () =>
+      import('./pages/contact-list/contact-list.module').then(
+        (m) => m.ContactListModule
+      ),
+  },
   {
     path: 'addcontact',
-    loadChildren: () => import('./pages/addcontact/addcontact.module').then(m => m.AddcontactModule) 
-  },  
- { 
-    path: 'editar/:id',
-    loadChildren: () => import('./pages/addcontact/addcontact.module').then(m => m.AddcontactModule)  
-  }, 
-  {
-    path:'',
-    pathMatch:'full',
-    redirectTo:'home'
+    loadChildren: () =>
+      import('./pages/addcontact/addcontact.module').then(
+        (m) => m.AddcontactModule
+      ),
   },
-  
   {
-    path:"**",
-    redirectTo:'home'
-  }
-  
+    path: 'editar/:id',
+    loadChildren: () =>
+      import('./pages/addcontact/addcontact.module').then(
+        (m) => m.AddcontactModule
+      ),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{preloadingStrategy:PreloadAllModules,useHash:true})],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/pages/contact-list/contact-list.component.ts
+++ src/app/pages/contact-list/contact-list.component.ts
@@ -1,27 +1,38 @@
-import { Component,  OnInit,} from '@angular/core';
-import { Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ContactosService } from 'src/app/shared/services/contactos.service';
-import { Contacto } from "../../shared/models/contacto";
+import Swal from 'sweetalert2';
+import { Contacto } from '../../shared/models/contacto';
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  styleUrls: ['./contact-list.component.css'],
 })
-export class ContactListComponent implements OnInit{
- 
-  constructor(public contactSVC:ContactosService, 
-              private router:Router) {}
+export class ContactListComponent implements OnInit {
+  constructor(public contactSVC: ContactosService, private router: Router) {}
 
   ngOnInit(): void {
-    this.contactSVC.cargarStorage()
-  };
-  
-  borrar(contact:Contacto){
-    this.contactSVC.eliminar(contact)
-  };
+    this.contactSVC.cargarStorage();
+  }
 
- pageEditar(some:string){
-  this.router.navigate(["/editar", some]) 
- };
-  
+  borrar(contact: Contacto) {
+    Swal.fire({
+      title: 'Eliminar Contacto?',
+      text: 'Estas Seguro Que Deseas Eliminar Este Contacto',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, Eliminar!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.contactSVC.eliminar(contact);
+        Swal.fire('Contacto Eliminado', 'success');
+      }
+    });
+  }
+
+  pageEditar(some: string) {
+    this.router.navigate(['/editar', some]);
+  }
 }
